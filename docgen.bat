986afde28d5925a5cd34f8740ca43d68d9ab6a19
Romain Deltour <rdeltour@gmail.com>
2010-12-23 13:21:39 +0100

    fixed *.bat script file

--- docgen.bat
+++ docgen.bat
@@ -6,7 +6,7 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 set CMD=%~n0
 set MODULE_DIR=%~dp0
-set COMMON_DIR=%MODULE_DIR%\..\common
+set COMMON_DIR=%MODULE_DIR%..\common
 set LIB_DIR=%COMMON_DIR%\lib
 set CONF_DIR=%COMMON_DIR%\conf
 set OUT_DIR=
@@ -49,10 +49,23 @@ IF "%SRC_DIR%"=="" (
 	ECHO.
 	GOTO Usage
 )
-IF "%OUT_DIR%"=="" (
-	set OUT_DIR=output
-)
-set CP=
-for %%f IN (%LIB_DIR%\*.jar) do set CP=!CP!;%%f
 
-%JAVA% -classpath %CP%  -Dcom.xmlcalabash.phonehome=false com.xmlcalabash.drivers.Main -c file:///%CONF_DIR:\=/%/calabash-config.xml %MODULE_DIR%\documenter-0.2\document.xpl src=%SRC_DIR:\=/% doc=%OUT_DIR:\=/%
+set URI_SPACE=%%20
+
+set CONF_CALABASH="file:///%CONF_DIR:\=/%/calabash-config.xml"
+set CONF_CALABASH=%CONF_CALABASH: =!URI_SPACE!%
+
+set SRC_DIR="%SRC_DIR:\=/%"
+set SRC_DIR=%SRC_DIR: =!URI_SPACE!%
+
+IF "%OUT_DIR%"=="" (
+	set OUT_FILE=output
+) ELSE (
+	set OUT_DIR="%OUT_DIR:\=/%"
+	set OUT_DIR=%OUT_DIR: =!URI_SPACE!%
+)
+
+set CP=
+for %%f IN ("%LIB_DIR%\*.jar") do set CP=!CP!;"%%f"
+
+%JAVA% -classpath %CP%  -Dcom.xmlcalabash.phonehome=false com.xmlcalabash.drivers.Main -c %CONF_CALABASH% "%MODULE_DIR%\documenter-0.2\document.xpl" src=%SRC_DIR% doc=%OUT_DIR%
