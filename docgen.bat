db9e161d96ee6cb65c40610ee31a33e3958a2611
Romain Deltour <rdeltour@gmail.com>
2010-12-23 19:54:24 +0100

    fixed several issues in launcher scripts related to spaces in file paths

--- docgen.bat
+++ docgen.bat
@@ -44,28 +44,41 @@ GOTO:EOF
 :Continue
 
 set SRC_DIR=%1
+
+set URI_SPACE=%%20
+
+:: Remove quotes in SRC_DIR
+SET SRC_DIR=###%SRC_DIR%###
+set SRC_DIR=%SRC_DIR:\=/%
+set SRC_DIR=%SRC_DIR: =!URI_SPACE!%
+SET SRC_DIR=%SRC_DIR:"###=%
+SET SRC_DIR=%SRC_DIR:###"=%
+SET SRC_DIR=%SRC_DIR:###=%
+
+:: Remove quotes in OUT_DIR
+SET OUT_DIR=###%OUT_DIR%###
+set OUT_DIR=%OUT_DIR:\=/%
+set OUT_DIR=%OUT_DIR: =!URI_SPACE!%
+SET OUT_DIR=%OUT_DIR:"###=%
+SET OUT_DIR=%OUT_DIR:###"=%
+SET OUT_DIR=%OUT_DIR:###=%
+
+
+set CONF_CALABASH="file:///%CONF_DIR:\=/%/calabash-config.xml"
+set CONF_CALABASH=%CONF_CALABASH: =!URI_SPACE!%
+
+
 IF "%SRC_DIR%"=="" (
 	ECHO The source directory must be set
 	ECHO.
 	GOTO Usage
 )
 
-set URI_SPACE=%%20
-
-set CONF_CALABASH="file:///%CONF_DIR:\=/%/calabash-config.xml"
-set CONF_CALABASH=%CONF_CALABASH: =!URI_SPACE!%
-
-set SRC_DIR="%SRC_DIR:\=/%"
-set SRC_DIR=%SRC_DIR: =!URI_SPACE!%
-
 IF "%OUT_DIR%"=="" (
-	set OUT_FILE=output
-) ELSE (
-	set OUT_DIR="%OUT_DIR:\=/%"
-	set OUT_DIR=%OUT_DIR: =!URI_SPACE!%
+	set OUT_DIR=output
 )
 
 set CP=
 for %%f IN ("%LIB_DIR%\*.jar") do set CP=!CP!;"%%f"
 
-%JAVA% -classpath %CP%  -Dcom.xmlcalabash.phonehome=false com.xmlcalabash.drivers.Main -c %CONF_CALABASH% "%MODULE_DIR%\documenter-0.2\document.xpl" src=%SRC_DIR% doc=%OUT_DIR%
+%JAVA% -classpath %CP%  -Dcom.xmlcalabash.phonehome=false com.xmlcalabash.drivers.Main -c %CONF_CALABASH% "%MODULE_DIR%\documenter-0.2\document.xpl" src="%SRC_DIR%" doc="%OUT_DIR%"
